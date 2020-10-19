0aa0acc97166949d304398c9ff72b1cafcb60fc9
jmperez <jmperez@WLPF1CVU6T.wks.jnj.com>
2020-10-19 18:32:57 +0200

    Tarea01 de la asignatura Programación

---
+++ PSJM_Online01/src/psjm_online01/PSJM_Online01.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package psjm_online01;
+
+/**
+ *
+ * @author jmperez
+ */
+public class PSJM_Online01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*Estoy abriendo un comenario que puede ocupar más de una línea, por si acaso.
+        Primero estoy diciéndoile al Sistema (System), que sace por la consola (out), una impresión
+        (print) y que lo haga en la línea siguiente (ln), y que esa impresión sea mi nombre*/
+        
+        System.out.println("José María Pérez Sarabia");
+        
+        //Inicio comentario de una línea. Declaro una variable entera int, luego la llamo número
+        int numero;
+        //Y ahora la inicializo dándole un valor
+        numero=100;
+        
+          // Y ahora le digo al sitema que sauqe por pantalla el valor de la variable numero  
+        
+        System.out.println(numero);
+                
+    }
+
+}
+
